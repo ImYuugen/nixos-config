337f71ae5080fda3ae4dedb59cbcb48e48f45784
Yuugen <yuugenssb@proton.me>
2024-02-23 12:58:28 +0100

    feat: add helix editor
    Signed-off-by: Yuugen <yuugenssb@proton.me>

--- modules/programs/common/editors/helix/default.nix
+++ modules/programs/common/editors/helix/default.nix
@@ -1,5 +1,21 @@
 { config, pkgs, ... }:
 
 {
-  #TODO
+  programs.helix = {
+    enable = true;
+    defaultEditor = true;
+    settings = {
+      theme = "catppuccin_mocha";
+      editor = {
+        line-number = "relative";
+        lsp = {
+          display-messages = true;
+          display-inlay-hints = true;
+        };
+        cursor-shape = {
+          insert = "bar";
+        };
+      };
+    };
+  };
 }
