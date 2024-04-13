aa179519097eab018557e69c3ec27beb96449f4d
Yuugen <yuugenssb@proton.me>
2024-04-13 23:44:53 +0200

    feat(dev): add clang again
    Signed-off-by: Yuugen <yuugenssb@proton.me>

--- modules/programs/common/dev/c_c++/default.nix
+++ modules/programs/common/dev/c_c++/default.nix
@@ -3,19 +3,35 @@
 {
   home.packages = with pkgs; [
     # Build tools
-    autoconf autoconf-archive automake
-    cmake gnumake meson ninja
+    autoconf
+    autoconf-archive
+    automake
+    cmake
+    gnumake
+    meson
+    ninja
 
     # Compilers
     (lib.setPrio (gcc.meta.priority + 1) gcc-unwrapped)
-    llvmPackages_14.llvm llvmPackages_14.lld
+    llvmPackages_14.clang
+    llvmPackages_14.lldb
+    llvmPackages_14.llvm
+    llvmPackages_14.lld
 
     # Testing
-    criterion gtest gcovr valgrind
+    criterion
+    gtest
+    gcovr
+    valgrind
 
     # Misc
-    clang-tools gdb doxygen pkg-config
-    httplib yaml-cpp bear
+    clang-tools
+    gdb
+    doxygen
+    pkg-config
+    httplib
+    yaml-cpp
+    bear
   ];
 
   home.file = {
