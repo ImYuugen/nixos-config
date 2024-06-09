573f037884aac71b78eb4ff5111b80e58ba41517
Yuugen <yuugenssb@proton.me>
2024-06-09 03:18:54 +0200

    feat(wayland): add default pkgs for wayland
    Signed-off-by: Yuugen <yuugenssb@proton.me>

---
+++ home/shared/wayland/default.nix
@@ -0,0 +1,25 @@
+{pkgsSet, ...}: {
+  nix = {
+    settings = {
+      substituters = ["https://hyprland.cachix.org"];
+      trusted-public-keys = ["hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="];
+    };
+  };
+
+  # Probably not useful
+  home.packages = with pkgsSet.stable; [
+    wayland
+    wayland-scanner
+    wayland-utils
+    xwayland
+    wlroots
+    wlrctl
+    wlr-protocols
+    wlr-randr
+    xdg-desktop-portal-wlr
+    wev
+    egl-wayland
+    glfw-wayland
+    qt6.qtwayland
+  ];
+}
