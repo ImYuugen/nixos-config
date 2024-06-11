4e00211101b44efb553f43b218be868556d12e80
Yuugen <yuugenssb@proton.me>
2024-06-11 02:24:47 +0200

    fix(easyeffects): presets in wrong location
    Signed-off-by: Yuugen <yuugenssb@proton.me>

--- home/shared/sound/easyeffects.nix
+++ home/shared/sound/easyeffects.nix
@@ -2,7 +2,7 @@
   home.packages = [ pkgsSet.stable.easyeffects ];
 
   xdg.configFile = {
-    "output/PerfectEQ.json".source = ./effects/PerfectEQ.json;
-    "output/AdvAutoGain.json".source = ./effects/AdvAutoGain.json;
+    "easyeffects/output/PerfectEQ.json".source = ./effects/PerfectEQ.json;
+    "easyeffects/output/AdvAutoGain.json".source = ./effects/AdvAutoGain.json;
   };
 }
