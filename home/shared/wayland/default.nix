e3a419dc3d36d944f14cfb7d0c0448f60a80d489
Yuugen <yuugenssb@proton.me>
2024-06-13 16:26:46 +0200

    fix: cache was overriden
    Signed-off-by: Yuugen <yuugenssb@proton.me>

--- home/shared/wayland/default.nix
+++ home/shared/wayland/default.nix
@@ -1,8 +1,8 @@
 { pkgsSet, ... }: {
   nix = {
     settings = {
-      substituters = [ "https://hyprland.cachix.org" ];
-      trusted-public-keys = [ "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc=" ];
+      extra-substituters = [ "https://hyprland.cachix.org" ];
+      extra-trusted-public-keys = [ "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc=" ];
     };
   };
 
