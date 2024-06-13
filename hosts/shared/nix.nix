dd222ff7f7eccd1aa017728a609c8fc7b0f6b69a
Yuugen <yuugenssb@proton.me>
2024-06-13 15:48:32 +0200

    refac: disable nix automatic optimise/gc
    Signed-off-by: Yuugen <yuugenssb@proton.me>

--- hosts/shared/nix.nix
+++ hosts/shared/nix.nix
@@ -7,7 +7,7 @@
 
   nix = {
     settings = {
-      auto-optimise-store = true;
+      auto-optimise-store = false;
       experimental-features = [
         "nix-command"
         "flakes"
@@ -16,15 +16,14 @@
       substituters = [
         "https://cache.nixos.org"
         "https://nix-community.cachix.org"
+        "https://s3.cri.epita.fr/cri-nix-cache.s3.cri.epita.fr"
       ];
       trusted-public-keys = [
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+        "cache.nix.cri.epita.fr:qDIfJpZWGBWaGXKO3wZL1zmC+DikhMwFRO4RVE6VVeo="
       ];
     };
-    gc = {
-      automatic = true;
-      dates = "weekly";
-      options = "--delete-older-than 7d";
-    };
+    gc.automatic = false;
+    optimise.automatic = false;
   };
 }
