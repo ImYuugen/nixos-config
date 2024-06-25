70bdeae067b22669bc2a15925191b7c03496da3f
Yuugen <yuugenssb@proton.me>
2024-06-25 22:03:50 +0200

    feat: add .xinitrc
    Signed-off-by: Yuugen <yuugenssb@proton.me>

--- home/shared/xorg/bspwm/default.nix
+++ home/shared/xorg/bspwm/default.nix
@@ -22,4 +22,17 @@
   xsession.windowManager.bspwm = {
     enable = true;
   };
+
+  home.file.".xinitrc".text = ''
+    if test -z "$DBUS_SESSION_BUS_ADDRESS"; then
+        eval $(dbus-launch --exit-with-session --sh-syntax)
+    fi
+    systemctl --user import-environment DISPLAY XAUTHORITY
+    if command -v dbus-update-activation-environment > /dev/null 2>&1; then
+        dbus-update-activation-environment DISPLAY XAUTHORITY
+    fi
+
+    sxhkd &
+    exec bspwm
+  '';
 }
