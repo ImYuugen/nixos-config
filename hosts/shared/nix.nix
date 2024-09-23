10545e55854a1fa6c809e5d608fb614a253a22ff
Yuugen <yuugenssb@proton.me>
2024-09-23 15:45:21 +0200

    refac: remove epita cache (no worky ?)
    Signed-off-by: Yuugen <yuugenssb@proton.me>

--- hosts/shared/nix.nix
+++ hosts/shared/nix.nix
@@ -16,11 +16,11 @@
       substituters = [
         "https://cache.nixos.org"
         "https://nix-community.cachix.org"
-        "https://s3.cri.epita.fr/cri-nix-cache.s3.cri.epita.fr"
+        # "https://s3.cri.epita.fr/cri-nix-cache.s3.cri.epita.fr"
       ];
       trusted-public-keys = [
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-        "cache.nix.cri.epita.fr:qDIfJpZWGBWaGXKO3wZL1zmC+DikhMwFRO4RVE6VVeo="
+        # "cache.nix.cri.epita.fr:qDIfJpZWGBWaGXKO3wZL1zmC+DikhMwFRO4RVE6VVeo="
       ];
     };
     gc.automatic = false;
