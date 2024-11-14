96c680db2603def028a3db56eeafca1b5c292885
Yuugen <yuugenssb@proton.me>
2024-11-14 10:41:38 +0100

    fix(hypr): fix env vars for EAC games
    Signed-off-by: Yuugen <yuugenssb@proton.me>

--- home/shared/wayland/hypr/settings.nix
+++ home/shared/wayland/hypr/settings.nix
@@ -18,9 +18,8 @@
     };
 
     env = [
-      "CLUTTER_BACKEND,wayland"
       "GDK_SCALE,1"
-      "GDK_BACKEND,wayland"
+      "GDK_BACKEND,wayland,x11,*"
       "LIBSEAT_BACKEND,logind"
       "LIBVA_DRIVER_NAME,radeonsi"
       "LIBVA_DRIVER_NAME,nvidia"
@@ -30,7 +29,7 @@
       "ELECTRON_OZONE_PLATFORM_HINT,auto"
       "NIXOS_OZONE_WL,1"
       "MOZ_ENABLE_WAYLAND,1"
-      "SDL_VIDEODRIVER,wayland"
+      "SDL_VIDEODRIVER,wayland,x11,windows" # Easy anti cheat
       "XDG_CURRENT_DESKTOP,Hyprland"
       "XDG_SESSION_DESKTOP,Hyprland"
       "XDG_SESSION_TYPE,wayland"
@@ -38,7 +37,7 @@
       "XCURSOR_SIZE,20"
       "QT_AUTO_SCREEN_SCALE_FACTOR,1"
       "QT_SCALE_FACTOR,1"
-      "QT_QPA_PLATFORM,wayland"
+      "QT_QPA_PLATFORM,wayland;xcb"
       "QT_QPA_PLATFORMTHEME,qt5ct"
       "QT_WAYLAND_DISABLE_WINDOWDECORATION,1"
       "WLR_DRM_DEVICES,$HOME/.config/hypr/amdgpu:$HOME/.config/hypr/nvidia"
