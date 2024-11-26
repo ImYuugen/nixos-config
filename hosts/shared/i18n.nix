b7a214db9ef3a50feaec50850919fd55d2dceae8
Yuugen <yuugenssb@proton.me>
2024-11-26 19:02:15 +0100

    fix(i18n): disable fcitx until bazel is fixed
    Signed-off-by: Yuugen <yuugenssb@proton.me>

--- hosts/shared/i18n.nix
+++ hosts/shared/i18n.nix
@@ -14,16 +14,17 @@ in
       FR
       JP
     ];
-    inputMethod = {
-      enable = true;
-      type = "fcitx5";
-      fcitx5 = {
-        addons = with pkgs; [
-          fcitx5-gtk
-          fcitx5-mozc
-          fcitx5-rose-pine
-        ];
-      };
-    };
+    # BAZEL IS BROKEN RAAAAH
+    # inputMethod = {
+    #   enable = true;
+    #   type = "fcitx5";
+    #   fcitx5 = {
+    #     addons = with pkgs; [
+    #       fcitx5-gtk
+    #       fcitx5-mozc
+    #       fcitx5-rose-pine
+    #     ];
+    #   };
+    # };
   };
 }
