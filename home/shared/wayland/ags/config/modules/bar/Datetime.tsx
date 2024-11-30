ee3477a8c2ea1a9d175b69fd576e00e203254e24
Yuugen <yuugenssb@proton.me>
2024-11-30 17:31:49 +0100

    feat(ags/datetime): datetime module blueprint
    Signed-off-by: Yuugen <yuugenssb@proton.me>

--- home/shared/wayland/ags/config/modules/bar/Datetime.tsx
+++ home/shared/wayland/ags/config/modules/bar/Datetime.tsx
@@ -3,14 +3,29 @@ import { Variable, GLib } from "astal";
 // TODO: Seconds toggle
 // Calendar
 
-function DatetimeModule({ format = "%H:%M - %A %e" }) {
-    const time = Variable<string>("").poll(
-        1000,
-        () => GLib.DateTime.new_now_local().format(format)!,
+function DatetimeModule() {
+    const time = Variable(GLib.DateTime.new_now_local()).poll(1000, () =>
+        GLib.DateTime.new_now_local(),
     );
+    const dayKanji = ["", "月", "火", "水", "木", "金", "土", "日"];
 
     return (
-        <label className="bar-datetime" onDestroy={() => time.drop()} label={time()} />
+        <box className="datetime-module">
+            <label
+                className="day-kanji"
+                label={time((t) => dayKanji[+(t.format("%u") ?? 0)])}
+            />
+            <box>
+                <label
+                    className="time"
+                    label={time((t) => t.format("%R") ?? "00:00")}
+                />
+                <label
+                    className="date small-text"
+                    label={time((t) => t.format("%d/%m") ?? "00/00")}
+                />
+            </box>
+        </box>
     );
 }
 
