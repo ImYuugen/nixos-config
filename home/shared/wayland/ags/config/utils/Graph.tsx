f6d6baa67a5b94c91982f25cc3fb08314655fabc
Yuugen <yuugenssb@proton.me>
2024-12-05 16:57:35 +0100

    feat(ags): add primitive Graph widget
    Signed-off-by: Yuugen <yuugenssb@proton.me>

---
+++ home/shared/wayland/ags/config/utils/Graph.tsx
@@ -0,0 +1,103 @@
+import { Binding } from "astal";
+import Cairo from "cairo";
+
+type Props = {
+    samples?: number;
+    className?: string;
+    value: Binding<number>;
+    maxValue?: number;
+    minValue?: number;
+};
+
+// TODO: Under-the-curve fill enum:
+//  - Full
+//  - Empty
+//  - Hatched
+
+// Binds to a value, drawing a point for each past value
+// - `value`: the binding
+// - `{max,min}Value`: how {high,low} it can go
+// - `sample`: the number of points on the graph
+// CSS values:
+// - `min-{width,height}`: the {width,height}. duh
+// - `color`: color of the under-the-curve area
+// - `border-color`: color of the curve
+// - `border-width`: width of the curve
+// - `background-color`: you'll NEVER guess
+function Graph(props: Props) {
+    const DATA_BUF_LIMIT = props.samples ?? 10;
+    const DEFAULT_MAX_VALUE = 100;
+    const DEFAULT_MIN_VALUE = 0;
+    const clamp = (num: number, min: number, max: number) =>
+        Math.max(Math.min(num, max), min);
+
+    return (
+        <drawingarea
+            className={props.className}
+            setup={(self) => {
+                let data = new Array(DATA_BUF_LIMIT).fill(DEFAULT_MIN_VALUE);
+                props.maxValue = props.maxValue ?? DEFAULT_MAX_VALUE;
+
+                // FIX: If the value is updated, but is the same as
+                // before, this won't be called, for now use delta
+                self.hook(props.value, () => {
+                    let val = props.value.get();
+                    if (Number.isNaN(val)) {
+                        val = DEFAULT_MIN_VALUE;
+                    }
+                    data.push(
+                        clamp(
+                            Math.round(val),
+                            props.minValue ?? DEFAULT_MIN_VALUE,
+                            props.maxValue ?? DEFAULT_MAX_VALUE,
+                        ),
+                    );
+                    data.shift();
+                    print(data);
+                    self.queue_draw();
+                });
+                self.connect("draw", (_, cr: Cairo.Context) => {
+                    const ctx = self.get_style_context();
+                    const width = ctx.get_property(
+                        "min-width",
+                        ctx.get_state(),
+                    ) as number;
+                    const height = ctx.get_property(
+                        "min-height",
+                        ctx.get_state(),
+                    ) as number;
+                    const curveWidth = ctx.get_border(ctx.get_state());
+                    const curveColor = ctx.get_border_color(ctx.get_state());
+                    const bgColor = ctx.get_background_color(ctx.get_state());
+                    const fgColor = ctx.get_color(ctx.get_state());
+                    self.set_size_request(width, height);
+
+                    // Set scale so that 1, 1 is bottom right and 0,0 is top left
+                    cr.scale(width, height);
+
+                    // Draw the curve
+                    cr.setSourceRGBA(
+                        curveColor.red,
+                        curveColor.green,
+                        curveColor.blue,
+                        curveColor.alpha,
+                    );
+                    // Divide original width by scale reduction factor => width / 1.0
+                    cr.setLineWidth(0.05);
+                    cr.moveTo(0.0, 1.0);
+                    let x = 0.0, y = 1.0 - data[0] / props.maxValue!;
+                    cr.lineTo(x, y);
+                    for (let i = 0; i < data.length; i++) {
+                        y = 1.0 - data[i] / props.maxValue!;
+                        x = i / data.length;
+                        cr.lineTo(x, y);
+                    }
+                    cr.lineTo(1.0, y);
+                    cr.stroke();
+                });
+            }}
+        />
+    );
+}
+
+export default Graph;
