8555de10c393f5c421662253ad1a0dd044f26073
Yuugen <yuugenssb@proton.me>
2025-02-19 19:06:46 +0100

    feat(emacs): too much for a single commit, read diffs
    Signed-off-by: Yuugen <yuugenssb@proton.me>

--- home/shared/editors/emacs/config/init.el
+++ home/shared/editors/emacs/config/init.el
@@ -1,3 +1,5 @@
+(benchmark-init/activate)
+
 (setq make-backup-files nil)
 (global-visual-line-mode 1)
 (menu-bar-mode -1)
@@ -8,11 +10,60 @@
 (setq visible-bell t)
 (setq ring-bell-function 'ignore)
 
-(add-to-list 'custom-theme-load-path "~/.emacs.d/themes")
-; Nothing ever happens
-(setq custom-safe-themes t)
-(load-theme 'rose-pine-color t nil)
+(use-package catppuccin-theme
+  :config
+  (setopt catppuccin-flavor 'macchiato)
+  (load-theme 'catppuccin :noconfirm))
+(add-hook 'server-after-make-frame-hook #'catppuccin-reload)
 (add-to-list 'default-frame-alist '(font . "Iosevka Nerd Font Mono"))
 
+(setq indent-bars-color '(highlight :face-bg t :blend 0.15)
+      indent-bars-pattern "."
+      indent-bars-width-frac 0.1
+      indent-bars-pad-frac 0.1
+      indent-bars-zigzag nil
+      indent-bars-color-by-depth '(:regexp "outline-\\([0-9]+\\)" :blend 1)
+      indent-bars-highlight-current-depth '(:blend 0.5)
+      indent-bars-display-on-blank-lines t)
+
 (require 'org)
-(org-babel-load-file "~/.emacs.d/keys.org")
+(org-babel-load-file (expand-file-name "keys.org" user-emacs-directory))
+(org-babel-load-file (expand-file-name "modeline.org" user-emacs-directory))
+(org-babel-load-file (expand-file-name "lsp.org" user-emacs-directory))
+
+;; TODO
+(use-package centaur-tabs
+  :config
+  (setq centaur-tabs-style "wave")
+  (setq centaur-tabs-gray-out-icons 'buffer)
+  (setq centaur-tabs-modified-marker t))
+
+;; For command autocompletions
+(setq vertico-count 20)
+(vertico-mode)
+(setq completion-styles '(orderless basic))
+(keymap-set vertico-map "TAB" #'minibuffer-complete)
+(marginalia-mode)
+
+;; Extremely essential if you jump around flakes/shells
+(direnv-mode)
+(direnv-envrc-mode)
+
+;; Pretty colors :3
+(global-colorful-mode)
+
+;; HACK: This is here to reload the current file, as filetype detection gets messed up on reload
+(defun revert-all-file-buffers ()
+  "Refreshes all open files"
+  (interactive)
+  (dolist (buf (buffer-list))
+    (let ((filename (buffer-file-name)))
+      (when (and filename (not (buffer-modified-p buf)))
+	(if (file-readable-p filename)
+	    (with-current-buffer buf
+	      (revert-buffer-quick t))
+	  (let (kill-buffer-query-functions)
+	    (kill-buffer buf)
+	    (message "Killed non-existing/unreadable file buffer: %s" filename))))))
+  (message "Finished reverting buffers containing unmodified files."))
+(revert-all-file-buffers)
--- home/shared/editors/emacs/default.nix
+++ home/shared/editors/emacs/default.nix
@@ -1,45 +1,60 @@
 { pkgsSet, ... }:
 
+let
+  emacsWithPackages = pkgsSet.stable.emacsWithPackagesFromUsePackage {
+    package = pkgsSet.stable.emacs-unstable;
+    config = ./config/init.el;
+    extraEmacsPackages =
+      epkgs: with epkgs; [
+        ace-window # OK
+        all-the-icons # OK
+        autothemer # OK
+        avy # OK
+        benchmark-init
+        catppuccin-theme # OK
+        centaur-tabs # OK
+        clipmon # NOPE
+        colorful-mode # OK
+        consult # OK
+        dashboard # NOPE
+        direnv # OK
+        doom-modeline # OK
+        emojify # NOPE
+        flycheck # NOPE
+        forge # NOPE
+        highlight-thing # NOPE
+        indent-bars # NOPE
+        lsp-bridge # OK-ISH
+        magit # NOPE
+        magit-todos # NOPE
+        marginalia # OK
+        meow # OK
+        multiple-cursors # OK-ISH
+        polymode # NOPE
+        projectile # NOPE
+        orderless # OK
+        org # OK
+        org-bullets # NOPE
+        org-preview-html # OK
+        org-roam # NOPE
+        rainbow-delimiters # NOPE
+        realgud # NOPE
+        smartparens # NOPE
+        tempel # NOPE
+        treemacs # NOPE
+        use-package # OK
+        vertico # OK
+        ws-butler # NOPE
+        zoxide # OK-ISH
+      ];
+  };
+in
 {
-  home.packages = [
-    (pkgsSet.stable.emacsWithPackagesFromUsePackage {
-      package = pkgsSet.stable.emacs-unstable;
-      config = ./config/init.el;
-      extraEmacsPackages =
-        epkgs: with epkgs; [
-          ace-window
-          all-the-icons
-          autothemer
-          avy
-          centaur-tabs
-          clipmon
-          colorful-mode
-          dashboard
-          doom-modeline
-          emojify
-          flycheck
-          highlight-thing
-          indent-bars
-          lsp-bridge
-          meow
-          multiple-cursors
-          polymode
-          projectile
-          rainbow-delimiters
-          realgud
-          smartparens
-          tempel
-          treemacs
-          use-package
-          vertico
-          vertico-prescient
-          ws-butler
-          zoxide
-        ];
-    })
-  ];
   services.emacs = {
     enable = true;
-    package = pkgsSet.stable.emacs-unstable;
+    package = emacsWithPackages;
+    client = {
+      enable = true;
+    };
   };
 }
