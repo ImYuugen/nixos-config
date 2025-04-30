d7e2a57c3d8b03736e55cab91cc2303eee7a4792
Yuugen <yuugenssb@proton.me>
2025-05-01 01:00:37 +0200

    fix(omen): fix wrong names for CPU_SCALING_GOVERNOR
    Signed-off-by: Yuugen <yuugenssb@proton.me>

--- hosts/omen/default.nix
+++ hosts/omen/default.nix
@@ -118,8 +118,8 @@ in
     tlp = {
       enable = true;
       settings = {
-        CPU_SCALING_GOVERNOR_ON_AC = "schedutil";
-        CPU_SCALING_GOVERNOR_ON_BAT = "conservative";
+        CPU_SCALING_GOVERNOR_ON_AC = "performance";
+        CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
         CPU_ENERGY_PERF_POLICY_ON_AC = "default";
         CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
         CPU_BOOST_ON_AC = "1";
