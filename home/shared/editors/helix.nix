ea3214bab0d9c378d5929a91f95990b38b91d5a2
Yuugen <yuugenssb@proton.me>
2025-05-11 00:32:58 +0200

    feat(helix): add formatters for js/ts, fix nixfmt
    Signed-off-by: Yuugen <yuugenssb@proton.me>

--- home/shared/editors/helix.nix
+++ home/shared/editors/helix.nix
@@ -1,4 +1,4 @@
-{ pkgsSet, ... }:
+{ pkgsSet, lib, ... }:
 {
   # home.sessionVariables.EDITOR = "hx";
 
@@ -18,28 +18,57 @@
         };
       };
     };
-    languages.language = [
-      {
-        name = "cpp";
-        auto-format = true;
-        formatter = {
-          command = "clang-format";
-        };
-        language-servers = [ "clangd" ];
-        indent = {
-          tab-width = 4;
-          unit = "\t";
-        };
-      }
-      {
-        name = "nix";
-        auto-format = true;
-        formatter = {
-          command = "nix fmt";
-        };
-        language-servers = [ "nil" ];
-      }
-    ];
+    languages.language =
+      let
+        prettierdCfg =
+          {
+            name,
+            ext ? name,
+          }:
+          {
+            inherit name;
+            auto-format = true;
+            formatter = {
+              command = "prettierd";
+              args = [ ".${ext}" ];
+            };
+          };
+      in
+      [
+        {
+          name = "cpp";
+          auto-format = true;
+          formatter = {
+            command = "clang-format";
+          };
+          language-servers = [ "clangd" ];
+          indent = {
+            tab-width = 4;
+            unit = "\t";
+          };
+        }
+        {
+          name = "nix";
+          auto-format = true;
+          formatter.command = lib.getExe pkgsSet.stable.nixfmt-rfc-style;
+          language-servers = [ "nil" ];
+        }
+        (prettierdCfg {
+          name = "typescript";
+          ext = "ts";
+        })
+        (prettierdCfg {
+          name = "javascript";
+          ext = "js";
+        })
+        (prettierdCfg { name = "json"; })
+        (prettierdCfg { name = "jsonc"; })
+        (prettierdCfg { name = "tsx"; })
+        (prettierdCfg { name = "jsx"; })
+        (prettierdCfg { name = "css"; })
+        (prettierdCfg { name = "html"; })
+        (prettierdCfg { name = "scss"; })
+      ];
   };
 
   specialisation = {
