b42e0484750e0b48a7fcaff8f5fe8faffe44db4d
Yuugen <yuugenssb@proton.me>
2025-05-16 21:23:09 +0200

    misc: remove ags while I rewrite widgets
    Signed-off-by: Yuugen <yuugenssb@proton.me>

--- home/shared/wayland/ags/default.nix
+++ home/shared/wayland/ags/default.nix
@@ -2,16 +2,8 @@
 {
   imports = [ inputs.ags.homeManagerModules.default ];
 
-  programs.ags = {
-    enable = true;
-    # configDir = ./config;
-    extraPackages = with pkgsSet.stable; [ nodejs ];
-  };
-
   home.packages = [
     pkgsSet.stable.dart-sass
     pkgsSet.stable.libdbusmenu-gtk3
   ];
-
-  wayland.windowManager.hyprland.settings.exec-once = [ "ags" ];
 }
