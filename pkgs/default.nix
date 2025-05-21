89dcd3b60fa66551df9c5f69d9c92db990b07962
Yuugen <yuugenssb@proton.me>
2025-05-21 19:16:03 +0200

    feat(something): add a random package, dw ab ts
    Signed-off-by: Yuugen <yuugenssb@proton.me>

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -2,4 +2,5 @@
 {
   slippi-launcher = pkgs.callPackage ./slippi.nix { };
   stm32cubeide = pkgs.callPackage ./stm32cubeide.nix { };
+  someRandomSoft = pkgs.callPackage ./someRandomSoft/package.nix { };
 }
