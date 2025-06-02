9bc6171c8e5017d307414518a3586f700244f533
Yuugen <yuugenssb@proton.me>
2025-06-02 04:03:42 +0200

    feat(yuugen): add git config
    Signed-off-by: Yuugen <yuugenssb@proton.me>

--- homes/yuugen/default.nix
+++ homes/yuugen/default.nix
@@ -1,7 +1,22 @@
 { self, ... }:
 
 {
-  imports = [
-    self.homeManagerModules.profiles.base
+  imports = with self.homeManagerModules; [
+    profiles.base
+    profiles.desktop.wayland.hyprland
+
+    programs.terminals.wezterm
+
+    ./git.nix
+  ];
+
+  modules = {
+    programs = {
+      terminals.wezterm.enable = true;
+    };
+  };
+
+  wayland.windowManager.hyprland.settings.bind = [
+    "SUPER, Return, exec, $TERMINAL"
   ];
 }
