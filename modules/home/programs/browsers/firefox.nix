84ad188e13a01353f8b03247f2cbdb920c087d7d
Yuugen <yuugenssb@proton.me>
2025-06-05 18:25:32 +0200

    feat(browser): add BROWSER env var
    Signed-off-by: Yuugen <yuugenssb@proton.me>

--- modules/home/programs/browsers/firefox.nix
+++ modules/home/programs/browsers/firefox.nix
@@ -10,9 +10,12 @@ in
 {
   options.modules.programs.browsers.firefox = {
     enable = lib.mkEnableOption "Firefox";
+    defaultBrowser = lib.mkEnableOption "Firefox as default browser";
   };
 
   config = lib.mkIf cfg.enable {
+    home.sessionVariables.BROWSER = lib.mkIf cfg.defaultBrowser "firefox";
+
     programs.firefox = {
       enable = lib.mkDefault true;
       # TODO: Default config
