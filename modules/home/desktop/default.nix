198a3070af270321d18a6deae24ee6003c2d0c03
Yuugen <yuugenssb@proton.me>
2025-06-08 04:16:03 +0200

    feat(dunst): add dunst
    Signed-off-by: Yuugen <yuugenssb@proton.me>

--- modules/home/desktop/default.nix
+++ modules/home/desktop/default.nix
@@ -1,6 +1,7 @@
 { lib, ... }:
 
 {
+  dunst = import ./dunst.nix;
   rofi = import ./rofi.nix;
   wayland = import ./wayland { inherit lib; };
 }
--- profiles/home/desktop/wayland/hyprland/default.nix
+++ profiles/home/desktop/wayland/hyprland/default.nix
@@ -9,14 +9,18 @@
   key = ./.;
 
   imports = [
+    self.homeManagerModules.desktop.dunst
     self.homeManagerModules.desktop.wayland.hyprland
 
     # Defaults I find nice
     ./settings.nix
   ];
 
-  modules.desktop.wayland.hyprland = {
-    enable = lib.mkDefault true;
-    autoDetectGPU = lib.mkDefault true;
+  modules.desktop = {
+    dunst.enable = true;
+    wayland.hyprland = {
+      enable = lib.mkDefault true;
+      autoDetectGPU = lib.mkDefault true;
+    };
   };
 }
