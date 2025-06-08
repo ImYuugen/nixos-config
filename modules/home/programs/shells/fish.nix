8899f458f8e9b70f8d8bbef2517684f046531cb4
Yuugen <yuugenssb@proton.me>
2025-06-09 00:57:25 +0200

    feat(fish): add config
    Signed-off-by: Yuugen <yuugenssb@proton.me>

--- modules/home/programs/shells/fish.nix
+++ modules/home/programs/shells/fish.nix
@@ -1,12 +1,17 @@
-{ config, lib, ... }:
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
 
 let
   cfg = config.modules.programs.shells.fish;
 in
 {
   options.modules.programs.shells.fish = {
-    enable = lib.mkEnableOption "Fish";
-    viBindings = lib.mkEnableOption "Vi keybinds";
+    enable = lib.mkEnableOption "My Fish config";
+    viBindings = lib.mkEnableOption "Vi bindings";
   };
 
   config = lib.mkIf cfg.enable {
@@ -14,9 +19,60 @@ in
 
     programs.fish = {
       enable = lib.mkDefault true;
-      interactiveShellInit = lib.mkDefault (
-        lib.strings.optionalString cfg.viBindings "fish_vi_key_bindings"
+      plugins =
+        let
+          mkFishPlugins = builtins.map (name: {
+            inherit name;
+            src = pkgs.fishPlugins.${name};
+          });
+        in
+        lib.mkDefault (mkFishPlugins [
+          "async-prompt"
+          "fzf-fish"
+          "plugin-git"
+          "puffer"
+          "sponge"
+        ]);
+      shellInit = lib.mkBefore ''
+        set sponge_successful_exit_codes 0 1 2
+        set sponge_allow_previously_successful true
+        set sponge_purge_only_on_exit
+      '';
+      interactiveShellInit = lib.mkBefore (
+        ''
+          set fish_greeting
+        ''
+        + lib.optionalString cfg.viBindings ''
+          set -g fish_key_bindings fish_vi_key_bindings
+        ''
       );
+      shellAbbrs = {
+        "gcsm" = lib.mkDefault "git commit -sm";
+        "gtree" = lib.mkDefault "eza -Tla --git-ignore --git";
+        "tree" = lib.mkDefault "eza -T";
+      };
+      shellAliases = {
+        "sudo" = lib.mkDefault "doas";
+        "nixamer" = lib.mkDefault "shutdown now";
+      };
+
+      functions = {
+        mkcd = lib.mkDefault ''
+          command mkdir $argv
+          if test $status = 0
+            switch $argv[(count $argv)]
+              case '-*'
+              case '*'
+                z $argv[(count $argv)]
+                return
+            end
+          end
+        '';
+
+        tardel = lib.mkDefault ''
+          tar -xvf $argv[1] && rm $argv[1]
+        '';
+      };
     };
   };
 }
