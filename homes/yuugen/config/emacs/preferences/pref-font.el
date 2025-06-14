ff4100d13ad6a9ecd01ab0a68fe745501e1d2cc8
Yuugen <yuugenssb@proton.me>
2025-06-14 20:20:09 +0200

    feat(emacs): add font from previous commit
    Signed-off-by: Yuugen <yuugenssb@proton.me>

--- homes/yuugen/config/emacs/preferences/pref-font.el
+++ homes/yuugen/config/emacs/preferences/pref-font.el
@@ -1,5 +1,11 @@
 ;; -*- coding: utf-8; lexical-binding: t -*-
 
-(set-face-attribute 'default nil :family "IosevkaNerdFontMono" :height 120)
+(defun font-exists-p (font)
+  (if (null (x-list-fonts font)) nil t))
+(when (window-system)
+  (cond ((font-exists-p "Moralerspace Neon")
+		 (set-frame-font "Moralerspace Neon:size=14" nil t))
+		((font-exists-p "IosevkaNerdFontMono")
+		 (set-frame-font "IosevkaNerdFontMono:size=14" nil t))))
 
 (provide 'pref-font)
