71696b37cab0367f3751a6a858f2346a584165dd
Yuugen <yuugenssb@proton.me>
2025-06-16 15:25:47 +0200

    feat(omen): automount shared partition
    Signed-off-by: Yuugen <yuugenssb@proton.me>

--- hosts/omen/disks.nix
+++ hosts/omen/disks.nix
@@ -10,6 +10,16 @@
       device = "/dev/disk/by-uuid/9DC3-35D5";
       fsType = "vfat";
     };
+    "/mnt/shared" = {
+      device = "/dev/disk/by-uuid/EE8C040B8C03CCD1";
+      fsType = "ntfs";
+      # Authorize for wheel group
+      options = [
+        "defaults"
+        "gid=1"
+        "umask=000"
+      ];
+    };
   };
   boot.initrd.luks.devices."luks-c939d0e2-c041-4aef-8f59-8cad5f1f7eae".device = "/dev/disk/by-uuid/c939d0e2-c041-4aef-8f59-8cad5f1f7eae";
 
