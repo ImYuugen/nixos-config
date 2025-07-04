86b1d2216a39c3f851b9d364b208c06aad3ff6ce
Yuugen <yuugenssb@proton.me>
2025-07-04 02:01:52 +0200

    feat(yuugen): add mango hud
    Signed-off-by: Yuugen <yuugenssb@proton.me>

--- homes/yuugen/gaming.nix
+++ homes/yuugen/gaming.nix
@@ -19,4 +19,37 @@
   home.packages = [
     pkgsSet.unstable.olympus
   ];
+
+  programs.mangohud = {
+    enable = true;
+    /*
+      settings =
+        let
+          # Takes [a b ...] and outputs an attrset with {a=true;b=true;...}
+          enableOpts = builtins.foldl' (acc: opt: acc // { "${opt}" = true; }) { };
+        in
+        (enableOpts [
+          "arch"
+          "cpu_stats"
+          "fps"
+          "frametime"
+          "fsr"
+          "gpu_stats"
+          "io_read"
+          "io_write"
+          "ram"
+          "refresh_rate"
+          "vram"
+          "vulkan_driver"
+          "wine"
+          "present_mode"
+        ])
+        // {
+          output_folder = "$HOME/Documents/MangoHud";
+          toggle_hud = "Shift_R+F12";
+          # General Options
+          fps_metrics = "avg,0.01";
+        };
+    */
+  };
 }
