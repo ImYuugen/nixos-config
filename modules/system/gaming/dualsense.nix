33d69399fe3152e5b75453532824b4d4f7d8911e
Yuugen <yuugenssb@proton.me>
2025-07-15 02:26:43 +0200

    fix(dualsense): remove mouse behaviour on every sony controller
    Signed-off-by: Yuugen <yuugenssb@proton.me>

--- modules/system/gaming/dualsense.nix
+++ modules/system/gaming/dualsense.nix
@@ -18,20 +18,16 @@ in
       pkgs = pkgsSet.stable;
       dualsense-udev = pkgs.writeTextDir "etc/udev/rules.d/70-dualsensectl.rules" ''
         # PS5 DualSense controller over USB hidraw
-        KERNEL=="hidraw*", ATTRS{idVendor}=="054c", ATTRS{idProduct}=="0ce6", MODE="0660", TAG+="uaccess"
-
+        KERNEL=="hidraw*", ATTRS{idVendor}=="054c", ATTRS{idProduct}=="0ce6", MODE="0660", TAG+="uaccess", ENV{LIBINPUT_IGNORE_DEVICE}="1"
         # PS5 DualSense controller over bluetooth hidraw
-        KERNEL=="hidraw*", KERNELS=="*054C:0CE6*", MODE="0660", TAG+="uaccess"
-
+        KERNEL=="hidraw*", KERNELS=="*054C:0CE6*", MODE="0660", TAG+="uaccess", ENV{LIBINPUT_IGNORE_DEVICE}="1"
         # PS5 DualSense Edge controller over USB hidraw
-        KERNEL=="hidraw*", ATTRS{idVendor}=="054c", ATTRS{idProduct}=="0df2", MODE="0660", TAG+="uaccess"
-
+        KERNEL=="hidraw*", ATTRS{idVendor}=="054c", ATTRS{idProduct}=="0df2", MODE="0660", TAG+="uaccess", ENV{LIBINPUT_IGNORE_DEVICE}="1"
         # PS5 DualSense Edge controller over bluetooth hidraw
-        KERNEL=="hidraw*", KERNELS=="*054C:0DF2*", MODE="0660", TAG+="uaccess"
+        KERNEL=="hidraw*", KERNELS=="*054C:0DF2*", MODE="0660", TAG+="uaccess", ENV{LIBINPUT_IGNORE_DEVICE}="1"
 
         # Disable PS4/5 touchpad acting as mouse
-        ATTRS{name}=="Sony Interactive Entertainment Wireless Controller Touchpad", ENV{LIBINPUT_IGNORE_DEVICE}="1"
-        ATTRS{name}=="Wireless Controller Touchpad", ENV{LIBINPUT_IGNORE_DEVICE}="1"
+        ACTION=="add|change", KERNEL=="event[0-9]*", ATTRS{name}=="*Wireless Controller Touchpad", ENV{LIBINPUT_IGNORE_DEVICE}="1"
       '';
     in
     {
