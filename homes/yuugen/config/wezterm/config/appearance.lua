b69da28995ea0df5a3cb9a8c9c9c1178bd2c7ee8
Yuugen <yuugenssb@proton.me>
2025-07-19 02:37:59 +0200

    feat(wezterm): add moralerspace font
    Signed-off-by: Yuugen <yuugenssb@proton.me>

--- homes/yuugen/config/wezterm/config/appearance.lua
+++ homes/yuugen/config/wezterm/config/appearance.lua
@@ -1,3 +1,5 @@
+local wezterm = require("wezterm")
+
 return {
   max_fps = 60,
   animation_fps = 60,
@@ -5,6 +7,11 @@ return {
   cursor_blink_ease_out = "EaseOut",
   default_cursor_style = "BlinkingBlock",
   cursor_blink_rate = 650,
+
+  font = wezterm.font_with_fallback({
+    "Moralerspace Argon NF",
+    "Noto Sans NF",
+  }),
 
   enable_tab_bar = true,
   hide_tab_bar_if_only_one_tab = true,
