9f31899848936b598e1e0997748fcddb01370645
Yuugen <yuugenssb@proton.me>
2025-07-27 18:38:08 +0200

    fix: switch usages of doas to sudo in build script
    Signed-off-by: Yuugen <yuugenssb@proton.me>

--- build-os.sh
+++ build-os.sh
@@ -19,7 +19,7 @@ shift
 case $action in
   switch)
     nixos-rebuild build --flake .#$host -j auto --fast -L -v --log-format internal-json 2>&1 | nom --json && \
-      doas ./result/bin/switch-to-configuration switch
+      sudo ./result/bin/switch-to-configuration switch
 
     if [ $? -ne 0 ]; then
       echo "System rebuild failed" 1>&2
@@ -28,7 +28,7 @@ case $action in
     ;;
   vm)
     # Leaving the old qcow might make you pull your hair out
-    rm omen.qcow2 2> /dev/null
+    rm $host.qcow2 2> /dev/null
     nix build .#nixosConfigurations.$host.config.system.build.vm -j auto -L -v --log-format internal-json 2>&1 | nom --json
     ;;
   *)
