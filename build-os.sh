02b94e8e2756e9f41f26a461302c1453f438f464
Yuugen <yuugenssb@proton.me>
2025-07-29 04:44:44 +0200

    fix(build): remove hardcoded sudo and doas
    Signed-off-by: Yuugen <yuugenssb@proton.me>

--- build-os.sh
+++ build-os.sh
@@ -16,10 +16,21 @@ shift
 action="$1"
 shift
 
+superuser_cmd=""
+if [ -x "$(command -v sudo)" ]; then
+  superuser_cmd="sudo"
+elif [ -x "$(command -v doas)" ]; then
+  superuser_cmd="doas"
+else
+  echo "Whatever you're using for privilege escalation is not supported.." 1>&2
+  exit 1
+fi
+echo "Using $superuser_cmd for privilege escalation"
+
 case $action in
   switch)
     nixos-rebuild build --flake .#$host -j auto --fast -L -v --log-format internal-json 2>&1 | nom --json && \
-      sudo ./result/bin/switch-to-configuration switch
+      "$superuser_cmd" ./result/bin/switch-to-configuration switch
 
     if [ $? -ne 0 ]; then
       echo "System rebuild failed" 1>&2
@@ -28,11 +39,11 @@ case $action in
     ;;
   vm)
     # Leaving the old qcow might make you pull your hair out
-    rm $host.qcow2 2> /dev/null
-    nix build .#nixosConfigurations.$host.config.system.build.vm -j auto -L -v --log-format internal-json 2>&1 | nom --json
+    rm "$host".qcow2 2> /dev/null
+    nix build .#nixosConfigurations."$host".config.system.build.vm -j auto -L -v --log-format internal-json 2>&1 | nom --json
     ;;
   *)
-    echo "You did not provide an option cuh (switch|vm)" >&2
+    echo "You did not provide an option cuh (switch|vm)" 1>&2
     exit 1
     ;;
 esac
